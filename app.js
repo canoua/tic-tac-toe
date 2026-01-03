c7971e21aa74d2c0ec62f20b125a3b707d25f0ae
unknown <akariaka@yandex.ru>
2026-01-04 02:08:26 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const cells = document.querySelectorAll('.cell');
+const cellsHeader = document.querySelector('.cells-header');
+const cellsWrapper = document.querySelector('.cells');
+const btn = document.getElementById('cells-header__btn');
+let clickCounter = 0;
+let checkedClick = false;
+
+
+cells.forEach(function(item) {
+  item.addEventListener('click', function(e) {
+    clickCounter++;
+    if(clickCounter%2==1) {
+      e.target.classList.add('round');
+      e.target.style.pointerEvents = 'none';
+    } else {
+      e.target.classList.add('cross');
+      e.target.style.pointerEvents = 'none';
+    }
+    
+    if(clickCounter <= 9) {
+      console.log(clickCounter);
+    }
+  })
+})
+
+btn.addEventListener('click', function(e) {
+  e.preventDefault();
+  cellsWrapper.classList.add('cells_active');
+  console.log(cellsHeader);
+  
+  cellsHeader.classList.add('hide');
+})
