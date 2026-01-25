9ab7399451ad91e5576a00ce4bd867eda3a9fecd
unknown <akariaka@yandex.ru>
2026-01-26 02:22:40 +0600

    done radiobutton

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,6 +1,14 @@
 import './HomePages.css'
+import { useState } from 'react'
 
 function HomePage() {
+  //option1 ставим по умолчанию
+  const [selectedRadio, setRadio] = useState('option1'); 
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRadio(event.target.value);
+  };
+  
   return (
     <>
       <div className="screen-1">
@@ -10,11 +18,25 @@ function HomePage() {
           <div className="cells__type-wrapper">
             <div className="cells__type">
               <label htmlFor="round">Нолики</label>
-              <input name="change" id="round" type="radio" checked />
+              <input 
+                name="change" 
+                id="round" 
+                type="radio" 
+                value="option1"
+                checked={selectedRadio==='option1'} 
+                onChange={handleChange}
+              />
             </div>
             <div className="cells__type">
               <label htmlFor="cross">Крестики</label>
-              <input name="change" id="cross" type="radio" />
+              <input 
+                name="change" 
+                id="cross" 
+                value="option2"
+                type="radio" 
+                checked={selectedRadio==='option2'} 
+                onChange={handleChange}
+              />
             </div>
           </div>
 
